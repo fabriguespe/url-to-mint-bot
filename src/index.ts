67a0f54f3f96aeb20f04cb4c510aa510ff44aa79
fabri <fguespe@gmail.com>
2024-10-20 16:06:14 -0700

    update

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 import { run, HandlerContext } from "@xmtp/message-kit";
 
-//Tracks conversation steps
-const inMemoryCacheStep = new Map<string, number>();
-
-//List of words to stop or unsubscribe.
-const stopWords = ["stop", "unsubscribe", "cancel", "list"];
-
 run(async (context: HandlerContext) => {
   const {
     client,
